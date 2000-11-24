84cf4b2061f3204e56ab21df039a77a7bc800bdb
hathawsh <hathawsh@localhost>
2000-11-24 17:46:27 +0000

    Redone; should now work.

--- Debugger/testrpc.py
+++ Debugger/testrpc.py
@@ -1,83 +1,40 @@
 
-import string, sys
-from ExternalLib.xmlrpclib import Server, Transport
+import os, sys
+sys.path[0:0] = [os.pardir]
 
-class TransportWithAuth (Transport):
+from ChildProcessClient import spawnChild
+from wxPython.wx import wxProcess
 
-    def request(self, host, handler, request_body):
-	# issue XML-RPC request
-
-	import httplib
-	h = httplib.HTTP(host)
-	h.putrequest("POST", handler)
-
-	# required by HTTP/1.1
-	h.putheader("Host", host)
-
-	# required by XML-RPC
-	h.putheader("User-Agent", self.user_agent)
-	h.putheader("Content-Type", "text/xml")
-	h.putheader("Content-Length", str(len(request_body)))
-
-        h.putheader('x-auth',
-                    '84637cf34613265706a9357df73a4d2c0ba74ca4')
-
-	h.endheaders()
-
-	if request_body:
-	    h.send(request_body)
-
-	errcode, errmsg, headers = h.getreply()
-
-	if errcode != 200:
-	    raise ProtocolError(
-		host + handler,
-		errcode, errmsg,
-		headers
-		)
-
-	return self.parse_response(h.getfile())
+class Monitor: pass
 
 
-from wxPython.wx import wxExecute, wxProcess, wxYield
-from xmlrpc import xmlrpclib
-import os, time
-
-def package_home(globals_dict):
-    __name__=globals_dict['__name__']
-    m=sys.modules[__name__]
-    if hasattr(m,'__path__'):
-        r=m.__path__[0]
-    elif "." in __name__:
-        r=sys.modules[__name__[:rfind(__name__,'.')]].__path__[0]
-    else:
-        r=__name__
-    if __name__ == '__main__':
-        return os.getcwd()
-    else:
-        return os.path.join(os.getcwd(), r)
-
-dsp = os.path.join(package_home(globals()), 'DebugServerProcess.py')
 process = wxProcess()
-##process.Redirect()
-wxExecute('%s "%s"' % (sys.executable, dsp), 0, process)
+process.Redirect()
 
-##line = ''
-##while string.find(line, '\n') < 0:
-##    stream = process.GetInputStream()
-##    if not stream.eof():
-##        text = stream.read()
-##        line = line + text
-##    else:
-##        wxYield()
+def pollStreams():
+    stream = process.GetInputStream()
+    if not stream.eof():
+        print stream.read()
+    stream = process.GetErrorStream()
+    if not stream.eof():
+        print stream.read()
 
-##port, auth = string.split(string.strip(line))
+print 'spawning...'
+s = spawnChild(Monitor(), process)
 
-port = 3245
-s = xmlrpclib.Server('http://localhost:%s' % port)
+print 'starting...'
+v = s.runFileAndRequestStatus('test.py', (), 0, (),
+                                ({'filename':'test.py',
+                                  'lineno':15,
+                                  'cond':'',
+                                  'enabled':1,
+                                  'temporary':0},
+                                 ))
+pollStreams()
+print v
+print 'running...'
+v = s.proceedAndRequestStatus('set_continue')
+pollStreams()
+print v
 
-time.sleep(5)
-
-print s.runFileAndRequestStatus(
-    os.path.join(package_home(globals()), 'test.py'), (), ())
-
+# Should stop in the middle of the process.
