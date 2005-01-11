79b35024874def8ab94267a246ead045eabdfa57
riaan <riaan@localhost>
2005-01-11 11:01:30 +0000

    Changes for wxPython 2.5

--- Plug-ins/UserCompanions.plug-in.py
+++ Plug-ins/UserCompanions.plug-in.py
@@ -15,7 +15,7 @@ from PropEdit import PropertyEditors
 import PaletteStore
 
 try:
-    import wxPython.lib.bcrtl
+    import wx.lib.bcrtl
 except ImportError:
     raise ImportError, 'The "bcrtl" package is not installed, turn on "installBCRTL" under Preferences'
 
@@ -23,26 +23,26 @@ except ImportError:
 
 # Objects which Boa will need at design-time needs to be imported into the
 # Companion module's namespace
-from wxPython.lib.bcrtl.user.ExampleST import *
+import wx.lib.bcrtl.user.ExampleST
 
 # Silly barebones example of a companion for a new component that is not
 # available in the wxPython distribution
 class ExampleSTDTC(BasicCompanions.StaticTextDTC):
     def writeImports(self):
-        return 'from wxPython.lib.bcrtl.user.ExampleST import *'
+        return 'import wx.lib.bcrtl.user.ExampleST.ExampleStaticText'
 
 #-------------------------------------------------------------------------------
 # Example of a composite control, control itself, implemented in
 # wxPython.lib.bcrtl.user.StaticTextCtrl
 
-from wxPython.lib.bcrtl.user.StaticTextCtrl import wxStaticTextCtrl
+import wx.lib.bcrtl.user.StaticTextCtrl
 
 class StaticTextCtrlDTC(BasicCompanions.TextCtrlDTC):
     def __init__(self, name, designer, parent, ctrlClass):
         BasicCompanions.TextCtrlDTC.__init__(self, name, designer, parent, ctrlClass)
         self.editors['CaptionAlignment'] = PropertyEditors.EnumPropEdit
-        self.options['CaptionAlignment'] = [wx.wxTOP, wx.wxLEFT]
-        self.names['CaptionAlignment'] = {'wx.TOP': wx.wxTOP, 'wx.LEFT': wx.wxLEFT}
+        self.options['CaptionAlignment'] = [wxTOP, wxLEFT]
+        self.names['CaptionAlignment'] = {'wx.TOP': wxTOP, 'wx.LEFT': wxLEFT}
 
     def constructor(self):
         return {'Value': 'value', 'Position': 'pos', 'Size': 'size',
@@ -50,7 +50,7 @@ class StaticTextCtrlDTC(BasicCompanions.TextCtrlDTC):
                 'Caption': 'caption'}
 
     def writeImports(self):
-        return 'from wxPython.lib.bcrtl.user.StaticTextCtrl import *'
+        return 'import wx.lib.bcrtl.user.StaticTextCtrl.StaticTextCtrl'
 
     def designTimeSource(self, position = 'wx.DefaultPosition', size = 'wx.DefaultSize'):
         dts = BasicCompanions.TextCtrlDTC.designTimeSource(self, position, size)
@@ -59,12 +59,17 @@ class StaticTextCtrlDTC(BasicCompanions.TextCtrlDTC):
 
 
 # Add the component's class to this list
-PaletteStore.paletteLists['User'].extend([wxExampleStaticText, wxStaticTextCtrl])
+PaletteStore.paletteLists['User'].extend([
+    wx.lib.bcrtl.user.ExampleST.ExampleStaticText, 
+    wx.lib.bcrtl.user.StaticTextCtrl.StaticTextCtrl])
 
 # Add an entry to this dict with the following structure:
 # <component class>: ['Palette tip name and bitmap file', <companion>]
-PaletteStore.compInfo.update({wxExampleStaticText: ['wxExampleStaticText', ExampleSTDTC],
-                              wxStaticTextCtrl: ['wxStaticTextCtrl', StaticTextCtrlDTC]})
+PaletteStore.compInfo.update({
+    wx.lib.bcrtl.user.ExampleST.ExampleStaticText: 
+        ['wxExampleStaticText', ExampleSTDTC],
+    wx.lib.bcrtl.user.StaticTextCtrl.StaticTextCtrl: 
+        ['wxStaticTextCtrl', StaticTextCtrlDTC]})
 
 #-------------------------------------------------------------------------------
 
@@ -72,7 +77,6 @@ PaletteStore.compInfo.update({wxExampleStaticText: ['wxExampleStaticText', Examp
 # integrating controls
 
 from wxPython.calendar import *
-from wxPython.utils import *
 
 class CalendarConstr:
     def constructor(self):
