ff6bed8b3142c59100985f15f73bc9b330bc3940
riaan <riaan@localhost>
2005-03-07 12:23:40 +0000

    Updated to browse new namespace

--- ClassBrowser.py
+++ ClassBrowser.py
@@ -1,119 +1,130 @@
 #----------------------------------------------------------------------
 # Name:        ClassBrowser.py
-# Purpose:     
+# Purpose:
 #
 # Author:      Riaan Booysen
 #
 # Created:     1999
 # RCS-ID:      $Id$
-# Copyright:   (c) 1999, 2000 Riaan Booysen
+# Copyright:   (c) 1999 - 2004 Riaan Booysen
 # Licence:     GPL
 #----------------------------------------------------------------------
+#Boa:Frame:ClassBrowserFrame
+
+import os, pyclbr
+
 from wxPython.wx import *
-import pyclbr
-import string
 
-def findInsertModules(name, tree):
-    ri = tree.GetRootItem()
-    item = ri
-    found = false
-    while item:
-        item = tree.GetNextSibling(item)
-        if tree.GetItemText(item) == name:
-            found = true
-            return item
-    
-    return tree.AddRoot(name)
+import wx
 
+import Preferences, Utils
+from Preferences import IS
 
-def travTilBase(name, classes, root):
-    if len(classes[name].super) == 0:
-	if not root.has_key(name):
-	    root[name] = {}
-	return root[name]
-    else:
-	c = travTilBase(classes[name].super[0].name, classes, root)
-        if not c.has_key(name):
-	    c[name] = {}
-	return c[name]
+[wxID_CLASSBROWSERFRAME, wxID_CLASSBROWSERFRAMEHIERARCHY,
+ wxID_CLASSBROWSERFRAMEPAGES, wxID_CLASSBROWSERFRAMESTATUSBAR,
+ wxID_CLASSBROWSERFRAMETREE,
+] = map(lambda _init_ctrls: wxNewId(), range(5))
 
-def buildTree(tree, parent, dict):
-    for item in dict.keys():
-        child = tree.AppendItem(parent, item)
-	if len(dict[item].keys()):
-	    buildTree(tree, child, dict[item])
-    
-def extractFilename(filename):
-    return filename[string.rfind(filename, '\\')+ 1: len(filename) -3]
-    
-class ClassBrowserFrame(wxFrame):
-    def __init__(self, parent, id, title):
-        wxFrame.__init__(self, parent, -1, title,
-                         wxPoint(100, 157), wxSize(200, 250))
+class ClassBrowserFrame(wxFrame, Utils.FrameRestorerMixin):
+    def _init_coll_pages_Pages(self, parent):
+        # generated method, don't edit
 
-        if wxPlatform == '__WXMSW__':
-            self.icon = wxIcon('Images/Icons/ClassBrowser.ico', wxBITMAP_TYPE_ICO)
-            self.SetIcon(self.icon)
+        parent.AddPage(select=true, imageId=-1, page=self.hierarchy,
+              text='Hierarchy')
+        parent.AddPage(select=false, imageId=-1, page=self.tree,
+              text='Modules')
 
-	self.classes = pyclbr.readmodule('wxPython.wx')
+    def _init_utils(self):
+        # generated method, don't edit
+        pass
 
-	self.pages = wxNotebook(self, -1)
-        self.statusBar = self.CreateStatusBar()
-#	self.panel = wxPanel(self, -1)
+    def _init_ctrls(self, prnt):
+        # generated method, don't edit
+        wxFrame.__init__(self, id=wxID_CLASSBROWSERFRAME, name='', parent=prnt,
+              pos=wxPoint(475, 238), size=wxSize(299, 497),
+              style=wxDEFAULT_FRAME_STYLE | Preferences.childFrameStyle,
+              title='wxPython Class Browser')
+        self._init_utils()
+        self.SetClientSize(wxSize(291, 470))
+        EVT_CLOSE(self, self.OnCloseWindow)
 
-        tID = NewId()
-        self.hierarchy = wxTreeCtrl(self.pages, tID)
-        self.pages.AddPage(self.hierarchy, 'Hierarchy')
-        wxYield() 
-        root = self.hierarchy.AddRoot("wxObject")
+        self.statusBar = wxStatusBar(id=wxID_CLASSBROWSERFRAMESTATUSBAR,
+              name='statusBar', parent=self, style=wxST_SIZEGRIP)
+        self.SetStatusBar(self.statusBar)
 
-	clsDict = {}
+        self.pages = wxNotebook(id=wxID_CLASSBROWSERFRAMEPAGES, name='pages',
+              parent=self, pos=wxPoint(0, 0), size=wxSize(291, 450), style=0)
 
-	for i in self.classes.keys():
-	    travTilBase(i, self.classes, clsDict)
+        self.hierarchy = wxTreeCtrl(id=wxID_CLASSBROWSERFRAMEHIERARCHY,
+              name='hierarchy', parent=self.pages, pos=wxPoint(0, 0),
+              size=wxSize(283, 424), style=wxTR_HAS_BUTTONS,
+              validator=wxDefaultValidator)
 
-	buildTree(self.hierarchy, root, clsDict)
+        self.tree = wxTreeCtrl(id=wxID_CLASSBROWSERFRAMETREE, name='tree',
+              parent=self.pages, pos=wxPoint(0, 0), size=wxSize(283, 424),
+              style=wxTR_HAS_BUTTONS, validator=wxDefaultValidator)
+
+        self._init_coll_pages_Pages(self.pages)
+
+    def __init__(self, parent):
+        self._init_ctrls(parent)
+
+        self.winConfOption = 'classbrowser'
+        self.loadDims()
+
+        self.SetIcon(IS.load('Images/Icons/ClassBrowser.ico'))
+
+        self.classes = {}
+        for module in ('wx', 'wx.html', 'wx.calendar', 'wx.grid', 'wx.stc',
+                       'wx.gizmos', 'wx.wizard'):
+                self.classes.update(pyclbr.readmodule(module))
+        
+
+        tID = wxNewId()
+        root = self.hierarchy.AddRoot('wxObject')
+
+        clsDict = {}
+
+        for i in self.classes.keys():
+            travTilBase(i, self.classes, clsDict)
+
+        buildTree(self.hierarchy, root, clsDict)
         self.hierarchy.Expand(root)
 
-        tID = NewId()
+        tID = wxNewId()
 
-        self.tree = wxTreeCtrl(self.pages, tID)
-        self.pages.AddPage(self.tree, 'Modules')
-        wxYield() 
-        
-        root = self.tree.AddRoot("Modules")
+        root = self.tree.AddRoot('Modules')
         modules = {}
         moduleName = ''
         for className in self.classes.keys():
-            moduleName = extractFilename(self.classes[className].file)
+            moduleName = os.path.basename(self.classes[className].file)
             if not modules.has_key(moduleName):
- 	        modules[moduleName] = {}
-	    modules[moduleName][className] = {}
-	    modules[moduleName][className]['Properties'] = {}
- 	    modules[moduleName][className]['Methods'] = {}
- 	    modules[moduleName][className]['Built-in'] = {}
-	    for method in self.classes[className].methods.keys():
+                modules[moduleName] = {}
+            modules[moduleName][className] = {}
+            modules[moduleName][className]['Properties'] = {}
+            modules[moduleName][className]['Methods'] = {}
+            modules[moduleName][className]['Built-in'] = {}
+            for method in self.classes[className].methods.keys():
                 if (method[:2] == '__'):
- 	            modules[moduleName][className]['Built-in'][method] = self.classes[className].lineno
+                    modules[moduleName][className]['Built-in'][method] = self.classes[className].lineno
                 elif (method[:3] == 'Get'):
                     if self.classes[className].methods.has_key('Set'+method[3:]):
- 	                modules[moduleName][className]['Properties'][method[3:]] = self.classes[className].lineno
-		    else:	
- 	                modules[moduleName][className]['Methods'][method] = self.classes[className].lineno
+                        modules[moduleName][className]['Properties'][method[3:]] = self.classes[className].lineno
+                    else:
+                        modules[moduleName][className]['Methods'][method] = self.classes[className].lineno
                 elif (method[:3] == 'Set'):
                     if self.classes[className].methods.has_key('Get'+method[3:]):
- 	                modules[moduleName][className]['Properties'][method[3:]] = self.classes[className].lineno
-		    else:	
- 	                modules[moduleName][className]['Methods'][method] = self.classes[className].lineno
+                        modules[moduleName][className]['Properties'][method[3:]] = self.classes[className].lineno
+                    else:
+                        modules[moduleName][className]['Methods'][method] = self.classes[className].lineno
                 else:
- 	            modules[moduleName][className]['Methods'][method] = self.classes[className].lineno
- 	
- 	moduleLst = modules.keys()
-	moduleLst.sort() 	               
-	for module in moduleLst:
+                    modules[moduleName][className]['Methods'][method] = self.classes[className].lineno
+        moduleLst = modules.keys()
+        moduleLst.sort()
+        for module in moduleLst:
             roots = self.tree.AppendItem(root, module)
- 	    classLst = modules[module].keys()
-	    classLst.sort() 	               
+            classLst = modules[module].keys()
+            classLst.sort()
             for classes in classLst:
                 aClass = self.tree.AppendItem(roots, classes)
                 methItem = self.tree.AppendItem(aClass, 'Methods')
@@ -127,10 +138,81 @@ class ClassBrowserFrame(wxFrame):
                     builtInItem = self.tree.AppendItem(bInItem, builtIns)
                 suprItem = self.tree.AppendItem(aClass, 'Super')
                 for supers in self.classes[classes].super:
-                    superItem = self.tree.AppendItem(suprItem, supers.name)
-                
-#                supers = self.tree.AppendItem(roots, 'Super')
-#                for super in self.classes[className].super:
-#                    aMethod = self.tree.AppendItem(supers, super.name)
+                    try:
+                        superItem = self.tree.AppendItem(suprItem, supers.name)
+                    except AttributeError:
+                        superItem = self.tree.AppendItem(suprItem, supers)
+
         self.tree.Expand(root)
-#	self.tree.Show(true)
+
+    def setDefaultDimensions(self):
+        self.SetDimensions(0, Preferences.underPalette,
+          Preferences.inspWidth,
+          Preferences.bottomHeight)
+
+    def OnCloseWindow(self, event):
+        self.Show(true)
+        self.Show(false)
+        if __name__ == '__main__':
+            self.Destroy()
+
+
+def findInsertModules(name, tree):
+    ri = tree.GetRootItem()
+    item = ri
+    found = false
+    while item:
+        item = tree.GetNextSibling(item)
+        if tree.GetItemText(item) == name:
+            found = true
+            return item
+
+    return tree.AddRoot(name)
+
+def travTilBase(name, classes, root):
+    if not classes.has_key(name):
+        if not root.has_key(name):
+            root[name] = {}
+        return root[name]
+    elif len(classes[name].super) == 0:
+        if not root.has_key(name):
+            root[name] = {}
+        return root[name]
+    else:
+        super1 = classes[name].super[0]
+        if type(super1) != type(''):
+            super1 = super1.name
+        c = travTilBase(super1, classes, root)
+        if not c.has_key(name):
+            c[name] = {}
+        return c[name]
+
+def buildTree(tree, parent, dict):
+    items = dict.keys()
+    items.sort()
+    for item in items:
+        child = tree.AppendItem(parent, item)
+        if len(dict[item].keys()):
+            buildTree(tree, child, dict[item])
+
+def openClassBrowser(editor):
+    palette = editor.palette
+    if not palette.browser:
+
+        wxBeginBusyCursor()
+        try:
+            palette.browser = ClassBrowserFrame(palette)
+        finally:
+            wxEndBusyCursor()
+    palette.browser.restore()
+
+    
+from Models import EditorHelper
+EditorHelper.editorToolsReg.append( ('wxPython class browser', openClassBrowser,
+      'Images/Shared/ClassBrowser.png') )
+        
+if __name__ == '__main__':
+    app = wxPySimpleApp()
+    frame = ClassBrowserFrame(None)
+    frame.Show(true)
+    app.MainLoop()
